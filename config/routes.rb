f7dd73c2b6b216179ca556ba3fcfa72100cd5f46
Ian Macalinao <me@ian.pw>
2015-04-25 02:49:55 -0500

    Change path in header for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get '/users/sign_in', to: redirect('/users/auth/facebook')
+  get '/users/sign_up', to: redirect('/users/auth/facebook')
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'home#index'
 
