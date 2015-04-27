bdda17eed8f8a23022b545bb18e9a18062955852
Ian Macalinao <me@ian.pw>
2015-04-27 15:05:15 -0500

    Count friends enrolled

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :schedules
   has_many :users, through: :schedules
 
-  def friends_enrolled(user = current_user)
-    @users & user.friends
+  def friends_enrolled(user)
+    self.users & user.friends
   end
 end
