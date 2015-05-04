476b25050dc76185243f1671f123cfc9f6ffc57e
Ian Macalinao <me@ian.pw>
2015-05-04 17:46:49 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426180021) do
+ActiveRecord::Schema.define(version: 20150504224020) do
 
-  create_table "Courses_Schedules", id: false, force: :cascade do |t|
-    t.integer "schedule_id", null: false
-    t.integer "course_id",   null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -35,7 +33,12 @@ ActiveRecord::Schema.define(version: 20150426180021) do
     t.integer  "semester_id"
   end
 
-  add_index "courses", ["semester_id"], name: "index_courses_on_semester_id"
+  add_index "courses", ["semester_id"], name: "index_courses_on_semester_id", using: :btree
+
+  create_table "courses_schedules", id: false, force: :cascade do |t|
+    t.integer "schedule_id", null: false
+    t.integer "course_id",   null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "user_id"
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20150426180021) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "schedules", ["semester_id"], name: "index_schedules_on_semester_id"
-  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id"
+  add_index "schedules", ["semester_id"], name: "index_schedules_on_semester_id", using: :btree
+  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id", using: :btree
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
@@ -60,7 +63,7 @@ ActiveRecord::Schema.define(version: 20150426180021) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "semesters", ["school_id"], name: "index_semesters_on_school_id"
+  add_index "semesters", ["school_id"], name: "index_semesters_on_school_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -82,7 +85,7 @@ ActiveRecord::Schema.define(version: 20150426180021) do
     t.string   "token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
